a39bb4271d6b3d513ec051da008aa90272a8c3ff
Manish <maknish1993@gmail.com>
2023-04-13 18:29:46 +0530

    JS file added

--- CalulatorApp/index.js
+++ CalulatorApp/index.js
@@ -1,1 +1,16 @@
-const display = 
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+// Define function to calculate based on button clicked
+const calculate = (btnValue) => {
+    console.log()
+}
+
+
+// Add eventlistener
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+})
+
+console.log("Manish Nishad")
