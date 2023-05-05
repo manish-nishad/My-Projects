465b95ed9dc6202bdfb8df08ead02360ee84fc06
Manish <maknish1993@gmail.com>
2023-05-05 18:58:01 +0530

    Navbar added

--- genius36/src/index.js
+++ genius36/src/index.js
@@ -7,9 +7,8 @@ import { ChakraProvider } from '@chakra-ui/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
-    
+    <ChakraProvider>
+      <App />
+    </ChakraProvider> 
   </>
 );
